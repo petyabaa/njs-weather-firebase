c9a454a7a5ab43a05452ac000578622604a5d9ef
Juhász-Nagy Péter <peti-mac@Juhasz-Nagy-Mac-mini.local>
2022-08-23 14:38:22 +0200

    Heroku add

--- public/js/app.js
+++ public/js/app.js
@@ -25,7 +25,7 @@ weatherForm.addEventListener('submit', (e) => {
     messageTwo.textContent = ''
     messageThree.textContent = ''
 
-    fetch('http://localhost:3000/weather?address=' + location).then((response) => {
+    fetch('/weather?address=' + location).then((response) => {
         response.json().then((data) => {
             if (data.error) {
                 /* console.error(data.error) */
